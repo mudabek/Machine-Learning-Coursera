262478a544f74a5325d0ddba288fecfdc15ab7f0
mudabek <nazarovo@grinnell.edu>
2019-08-08 23:37:17 +0500

    weird function

--- ex3/machine-learning-ex3/ex3/oneVsAll.m
+++ ex3/machine-learning-ex3/ex3/oneVsAll.m
@@ -50,6 +50,16 @@ X = [ones(m, 1) X];
 %
 
 
+initial_theta = zeros(n + 1, 1);
+options = optimset('GradObj', 'on', 'MaxIter', 50);
+c = 1:10;
+
+
+for c=1:num_labels
+    [theta] = ...
+         fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
+            initial_theta, options);
+endfor
 
 
 
